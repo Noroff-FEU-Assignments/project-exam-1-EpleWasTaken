cab448d7ca508b091bf6de700132c0fff9975822
Eple <71262074+EpleWasTaken@users.noreply.github.com>
2021-05-03 10:23:44 +0200

    Add files via upload

---
+++ js/alert.js
@@ -0,0 +1,6 @@
+// const formSent = document.getElementById("submitF");
+
+// formSent.addEventListener("click", () => {
+//     formSent.innerHTML = `Form sent`;
+//     formSent.style.backgroundColor = "rgb(70, 255, 70)";
+// });
